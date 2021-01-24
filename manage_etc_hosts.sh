1aeeed5efa7b3d3d11fe5f0648f51c41535696e9
premkiran7 <premkiran7@gmail.com>
2021-01-24 10:47:13 +0530

    added shell script to add, remove hostnames from /etc/hosts

---
+++ manage_etc_hosts.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+: '
+This script has been taken from https://gist.github.com/irazasyed/a7b0a079e7727a4315b9
+and some small modifications have been made. It can be used to add/remove a hostname to the
+/etc/hosts file.
+
+Usage (e.g):
+
+sh manage_etc_hosts.sh remove twitter.com
+sh manage_etc_hosts.sh add twitter.com
+'
+
+# Path to your hosts file
+hostsFile="/etc/hosts"
+
+# Default IP address for host
+ip="0.0.0.0"
+
+# Hostname to add/remove.
+hostname="$2"
+
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "cannot $*"; }
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+   die "Exiting... invalid arguments";
+fi
+
+remove() {
+    if [ -n "$(grep -w "$hostname$" /etc/hosts)" ]; then
+        echo "$hostname found in $hostsFile. Removing now...";
+       	try sudo sed -ie "/[[:space:]]$hostname/d" "$hostsFile";
+    else
+        yell "$hostname was not found in $hostsFile";
+    fi
+}
+
+add() {
+    if [ -n "$(grep -P "[[:space:]]$hostname" /etc/hosts)" ]; then
+        yell "$hostname, already exists: $(grep $hostname $hostsFile)";
+    else
+        echo "Adding $hostname to $hostsFile...";
+        try printf "%s\t\t%s\n" "$ip" "$hostname" | sudo tee -a "$hostsFile" > /dev/null;
+
+        if [ -n "$(grep $hostname /etc/hosts)" ]; then
+            echo "$hostname was added succesfully:";
+            echo "$(grep $hostname /etc/hosts)";
+        else
+            die "Failed to add $hostname";
+        fi
+    fi
+}
+
+$@
